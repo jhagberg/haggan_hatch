0d74ab8ac19d94018d86b2988fda22b983cf70ab
jonas <jonas@jonashagberg.se>
2014-05-15 08:45:27 +0200

    added web parsing of functions

--- haggan_hatch.ino
+++ haggan_hatch.ino
@@ -18,12 +18,18 @@ Servo myservo;
 int pos = 93;    // variable to store the servo position 
 int sign =1;
 unsigned long lastMillis;
-double Setpoint, Input, Output;
+double Setpoint, Input,Output;
 float humidity;
-double underKp=350, underKi=0, underKd=0;
-double consKp=350, consKi=0.35, consKd=0;
+double underKp=500, underKi=0, underKd=0;
+//double consKp=350, consKi=0.35, consKd=0;
+//double consKp=350, consKi=0.35, consKd=0;
+double consKp=400, consKi=0, consKd=0;
+//double consKp=2469.31, consKi=3133.83, consKd=1056.93;
+//2546.48 ki: 88.83
+//2546.48 ki: 97.09 kd: 0.00
 
-PID myPID(&Input, &Output, &Setpoint,350,0,0, DIRECT);
+
+PID myPID(&Input, &Output, &Setpoint,underKp,underKi,underKd, DIRECT);
 
 #define PIN_HIH4030 A0
 #define ONE_WIRE_BUS 5
@@ -80,6 +86,61 @@ Timezone CE(CEST, CET);
 #define PREFIX ""
 WebServer webserver(PREFIX, 80);
 
+#define NAMELEN 32
+#define VALUELEN 32
+
+void parsedCmd(WebServer &server, WebServer::ConnectionType type, char *url_tail, bool tail_complete)
+{
+  URLPARAM_RESULT rc;
+  char name[NAMELEN];
+  char value[VALUELEN];
+  double webP,webI,webD;
+  webP=myPID.GetKp();
+  webI=myPID.GetKi();
+  webD=myPID.GetKd();
+
+  P(Page_start) = "<html><head><title>Web_Parms_1 Version </title></head><body>\n";
+  P(Page_end) = "</body></html>";
+  P(Params_end) = "End of parameters<br>\n";
+  P(Parsed_item_separator) = " = '";
+  /* this line sends the standard "we're all OK" headers back to the
+   browser */
+  server.httpSuccess();
+
+  /* if we're handling a GET or POST, we can output our data here.
+   For a HEAD request, we just stop after outputting headers. */
+  if (type == WebServer::HEAD)
+    return;
+
+  server.printP(Page_start);
+
+  if (strlen(url_tail))
+  {
+    while (strlen(url_tail))
+    {
+      rc = server.nextURLparam(&url_tail, name, NAMELEN, value, VALUELEN);
+      if (rc == URLPARAM_EOS)
+        server.printP(Params_end);
+      else
+      {
+        server.print(name);
+        server.printP(Parsed_item_separator);
+        server.print(value);
+        if(name == "MODE")myPID.SetMode(int(value));
+        if(name == "OUTPUT")Output=strtod(value,NULL);
+        if(name == "P")webP=strtod(value,NULL);
+        if(name == "I")webI=strtod(value,NULL);
+        if(name == "D")webD=strtod(value,NULL);
+        
+      }
+    }
+  }
+
+  server.printP(Page_end);
+  
+  myPID.SetTunings(webP, webI, webD);
+
+}
 
 /* commands are functions that get called by the webserver framework
  * they can read any posted data from client, and they output to the
@@ -112,35 +173,18 @@ void sendTempHum(WebServer &server, WebServer::ConnectionType type, char *, bool
 }
 void helloCmd(WebServer &server, WebServer::ConnectionType type, char *, bool)
 {
-  /* this line sends the standard "we're all OK" headers back to the
-   browser */
   server.httpSuccess();
-
-
-  /* if we're handling a GET or POST, we can output our data here.
-   For a HEAD request, we just stop after outputting headers. */
   if (type != WebServer::HEAD)
   {
-    /* this defines some HTML text in read-only memory aka PROGMEM.
-     * This is needed to avoid having the string copied to our limited
-     * amount of RAM. */
-    //   P(helloMsg) = "<h1>Hello, World DEG!</h1><h1>";
-
-
-    /* this is a special form of print that outputs from PROGMEM */
-    // server.printP(helloMsg);
+    
     P(helloMsg)=
-    "<!DOCTYPE html><html><head><meta http-equiv=\"content-type\" content=\"text/html; charset=UTF-8\">"
-    "<title>Haggans Egg Hatcher</title>"
-    "<script type='text/javascript' src='http://192.168.1.147/jquery-2.1.0.js'></script>"
-    "<script src=\"http://192.168.1.147/highcharts.js\"></script>"
-    "<script src=\"http://192.168.1.147/egg.js\"></script>"
-    "</head><body><h1>Haggans Egg hatcher </h1><div id=\"container\" style=\"height: 400px\"></div></body></html>";
+      "<!DOCTYPE html><html><head><meta http-equiv=\"content-type\" content=\"text/html; charset=UTF-8\">"
+      "<title>Haggans Egg Hatcher</title>"
+      "<script type='text/javascript' src='http://192.168.1.147/jquery-2.1.0.js'></script>"
+      "<script src=\"http://192.168.1.147/highcharts.js\"></script>"
+      "<script src=\"http://192.168.1.147/egg.js\"></script>"
+      "</head><body><h1>Haggans Egg hatcher </h1><div id=\"container\" style=\"height: 400px\"></div></body></html>";
     server.printP(helloMsg);
-   // server.print(minute());
-
-
-
   }
 }
 
@@ -152,7 +196,7 @@ void setup()
   myservo.write(93);
   pinMode(FanPin, OUTPUT); 
   pinMode(RelayPin, OUTPUT); 
-  Setpoint = 37.45;
+  Setpoint = 37.44;
   lastMillis = millis(); 
   //HUM
   HIH4030::setup(PIN_HIH4030);
@@ -171,30 +215,23 @@ void setup()
     for(;;)
       ;
   }
-  // print your local IP address:
-  /*Serial.print("My IP address: ");
-   for (byte thisByte = 0; thisByte < 4; thisByte++) {
-   // print the value of each byte of the IP address:
-   Serial.print(Ethernet.localIP()[thisByte], DEC);
-   Serial.print("."); 
-   }
-   Serial.println();
-   */
+  
   Udp.begin(localPort);
 
   //Serial.println("waiting for sync");
   setSyncProvider(getNtpTime);
   while(timeStatus()== timeNotSet)   
     ; // wait until the time is set by the sync provider
-    
-  
-  
-   windowStartTime = millis();
-  
+
+
+
+  windowStartTime = millis();
+
 
   /* setup our default command that will be run when the user accesses
    * the root page on the server */
   webserver.setDefaultCommand(&helloCmd);
+//  webserver.addCommand("set", &parsedCmd);
 
   /* run the same command if you try to load /index.html, a common
    * default page name */
@@ -202,7 +239,7 @@ void setup()
 
   /* start the webserver */
   webserver.begin();
-  myPID.SetSampleTime(4000);
+  //  myPID.SetSampleTime(2000);
   //tell the PID to range between 0 and the full window size
   myPID.SetOutputLimits(0, 1000);
 
@@ -214,7 +251,7 @@ void setup()
 
 void loop()
 {
-  if(Input>30)myPID.SetOutputLimits(0, 600);
+  if(Input>32)myPID.SetOutputLimits(0, 600);
 
   if(Input>37)myPID.SetOutputLimits(0, 500);
 
@@ -227,8 +264,7 @@ void loop()
     Input = sensors.getTempCByIndex(0); 
     humidity=HIH4030::read(PIN_HIH4030, Input);
     FlexiTimer2::start();
-    if(Input!=85 || Input!=-127)
-    {myPID.Compute();}
+
   }
 
   if (millis() - lastMillis > 2000 && sign!= 0)
@@ -269,7 +305,7 @@ void loop()
   //TurnEggs();
   //char buff[64];
   //int len = 64;
-  
+
   /* process incoming connections one at a time forever */
   webserver.processConnection();
 }
@@ -303,6 +339,10 @@ void relayInterupt() {
   else if (Output >30 )
   {
     digitalWrite(RelayPin,HIGH);
+  }
+  if(Input!=85 || Input!=-127)
+  {
+    myPID.Compute();
   }
 
 }
@@ -363,4 +403,5 @@ unsigned long sendNTPpacket(IPAddress& address)
   Udp.write(packetBuffer,NTP_PACKET_SIZE);
   Udp.endPacket(); 
 }
+
 
